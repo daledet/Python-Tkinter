cb72f464684b0a0c3be52c5152a500912125a211
Dwight Ledet <dwightledet@gmail.com>
2020-07-27 16:02:03 +0200

    add calculator

--- button.py
+++ button.py
@@ -2,8 +2,13 @@ from tkinter import *
 
 root = Tk()
 
+e = Entry(root, width=50)
+e.pack()
+e.insert(0, "Enter Your Name: ")
+
 
 def myClick():
+    hello = "Hello " + e.get()
     myLabel = Label(root, text="Done")
     myLabel.pack()
 
