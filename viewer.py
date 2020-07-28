03a93bdcd9f48e7b17f859c201475def18fbbefb
Dwight Ledet <dwightledet@gmail.com>
2020-07-28 07:57:51 +0200

    add images

--- viewer.py
+++ viewer.py
@@ -7,11 +7,11 @@ root.title('Images')
 root.iconbitmap('img/ecco.png')
 
 # Define Image
-my_img1 = ImageTk.PhotoImage(Image.open("img/ecco.ico"))
-my_img2 = ImageTk.PhotoImage(Image.open("img/ecco.ico"))
-my_img3 = ImageTk.PhotoImage(Image.open("img/ecco.ico"))
-my_img4 = ImageTk.PhotoImage(Image.open("img/ecco.ico"))
-my_img5 = ImageTk.PhotoImage(Image.open("img/ecco.ico"))
+my_img1 = ImageTk.PhotoImage(Image.open("img/img1.jpg"))
+my_img2 = ImageTk.PhotoImage(Image.open("img/img2.jpg"))
+my_img3 = ImageTk.PhotoImage(Image.open("img/img3.jpg"))
+my_img4 = ImageTk.PhotoImage(Image.open("img/img4.jpg"))
+my_img5 = ImageTk.PhotoImage(Image.open("img/img5.jpg"))
 
 # Image List
 
@@ -22,9 +22,52 @@ image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
 
-button_back = Button(root, text="<<")
+# Functions
+
+
+def forward(image_number):
+    global my_label
+    global button_forward
+    global button_back
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(
+        root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(
+        root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number == 5:
+        button_forward = Button(root, text=">>", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+
+
+def back(image_number):
+    global my_label
+    global button_forward
+    global button_back
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(
+        root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(
+        root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number == 1:
+        button_back = Button(root, text="<<", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+
+
+button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_exit = Button(root, text="Exit", command=root.quit)
-button_forward = Button(root, text=">>")
+button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
